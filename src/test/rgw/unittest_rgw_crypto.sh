0f1b9a83dd7fba9999bed92c2bc6f5dd1d6daf36
Adam Kupczyk <akupczyk@mirantis.com>
2017-05-30 13:48:27 +0200

    radosgw, crypto: fixed test
    Signed-off-by: Adam Kupczyk <akupczyk@mirantis.com>

--- src/test/rgw/unittest_rgw_crypto.sh
+++ src/test/rgw/unittest_rgw_crypto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+set -e
+
 source $(dirname $0)/../detect-build-env-vars.sh
 $CEPH_BIN/unittest_rgw_crypto --plugin_crypto_accelerator=none
 $CEPH_BIN/unittest_rgw_crypto --plugin_crypto_accelerator=crypto_isal
