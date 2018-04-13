c819be183df52fe71e7175b40d3bd7dc3b2758eb
Adam Kupczyk <akupczyk@redhat.com>
2018-04-13 08:56:04 +0200

    bluestore/test: Fixed allocator test; fragmentation tests skipped for StupidAllocator.
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- src/test/objectstore/test_stupid_allocator.cc
+++ src/test/objectstore/test_stupid_allocator.cc
@@ -151,6 +151,95 @@ TYPED_TEST(AllocatorTest, test_add_free_rm_free_Fibonnaci_CantorSet) {
   }
 }
 
+
+TYPED_TEST(AllocatorTest, test_fragmentation) {
+  if (std::is_same<typename TestFixture::Allocator, StupidAllocator>::value) {
+    std::cerr << "[ SKIPPING ] --would fail--" << std::endl;
+    return;
+  }
+  typename TestFixture::Allocator sa(g_ceph_context);
+  constexpr size_t M = 1<<20;
+  constexpr size_t P = 4096;
+  sa.init_add_free(0, M);
+
+  for (int i=0; i<100;i++) {
+    uint64_t offset;
+    uint32_t v = rand() % (M / P);
+    uint32_t scale = cbits(M / P) - cbits(v);
+    uint64_t size = ( (rand() % (1 << scale)) + 1 ) * P;
+    if (0 == sa.reserve(size)) {
+      uint32_t allocated = 0;
+      if (0 == sa.allocate_int(size, P, 0, &offset, &allocated)) {
+        interval_set<uint64_t> tr;
+        tr.insert(offset, allocated);
+        sa.release(tr);
+      }
+      sa.unreserve(size - allocated);
+    }
+    ASSERT_EQ(sa.get_free(), M);
+  }
+  sa.reserve(M);
+  uint64_t offset;
+  uint32_t allocated;
+  ASSERT_EQ(0, sa.allocate_int(M, P, 0, &offset, &allocated));
+  ASSERT_EQ(allocated, M);
+}
+
+
+
+TYPED_TEST(AllocatorTest, test_fragmentation_dragged) {
+  if (std::is_same<typename TestFixture::Allocator, StupidAllocator>::value) {
+    std::cerr << "[ SKIPPING ] --would fail--" << std::endl;
+    return;
+  }
+  typename TestFixture::Allocator sa(g_ceph_context);
+  constexpr size_t M = 1<<26;
+  constexpr size_t P = 4096;
+  sa.init_add_free(0, M);
+  typedef std::pair<uint64_t, uint32_t> allocation_t;
+  typedef std::list<allocation_t> allocation_list_t;
+  allocation_list_t allocation_list;
+  std::vector<allocation_list_t::iterator> allocation_vector;
+
+  for (int i=0; i<10000;i++) {
+    uint64_t offset;
+    uint32_t v = rand() % (M / P);
+    uint32_t scale = cbits(M / P) - cbits(v);
+    uint64_t size = ( (rand() % (1 << scale)) + 1 ) * P;
+    if (0 == sa.reserve(size)) {
+      uint32_t allocated = 0;
+      if (0 == sa.allocate_int(size, P, 1, &offset, &allocated)) {
+        auto n = allocation_list.emplace(allocation_list.end(), allocation_t{offset,allocated});
+        allocation_vector.push_back(n);
+      }
+      sa.unreserve(size - allocated);
+      if (allocation_vector.size() > 100) {
+        size_t r = rand()%allocation_vector.size();
+        auto it = allocation_vector[r];
+        interval_set<uint64_t> tr;
+        tr.insert(it->first, it->second);
+        sa.release(tr);
+        allocation_vector[r] = allocation_vector.back();
+        allocation_vector.resize(allocation_vector.size() - 1);
+      }
+    }
+  }
+  interval_set<uint64_t> tr;
+  for (size_t i = 0; i < allocation_vector.size(); i++) {
+    auto it = allocation_vector[i];
+    tr.insert(it->first, it->second);
+  }
+  sa.release(tr);
+
+  ASSERT_EQ(sa.get_free(), M);
+
+  sa.reserve(M);
+  uint64_t offset;
+  uint32_t allocated;
+  ASSERT_EQ(0, sa.allocate_int(M, P, 0, &offset, &allocated));
+  ASSERT_EQ(allocated, M);
+}
+
 int main(int argc, char **argv) {
   vector<const char*> args;
   argv_to_vec(argc, (const char **)argv, args);
