142d6437a22e537ecca31c3509a11aeac384d08e
Adam Kupczyk <akupczyk@ibm.com>
2024-03-20 08:17:21 +0000

    os/bluestore: add do_write_v2_compressed()
    Modify do_write_v2() to handle also compressed.
    Segmented and regular cases are recognized and handled properly.
    New do_write_v2_compressed() oversees compression / recompression.
    
    Added split_and_compress() function.
    It is dumb now and always assumes compression makes sense.
    To be improved.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -269,6 +269,7 @@ public:
   struct Onode;
   class Scanner;
   class Estimator;
+
   typedef boost::intrusive_ptr<Collection> CollectionRef;
   typedef boost::intrusive_ptr<Onode> OnodeRef;
 
@@ -3307,6 +3308,13 @@ private:
     uint32_t op_flags = 0,
     uint64_t retry_count = 0);
 
+  void _do_read_and_pad(
+    Collection* c,
+    OnodeRef& o,
+    uint32_t offset,
+    uint32_t length,
+    ceph::buffer::list& bl);
+
   int _do_readv(
     Collection *c,
     OnodeRef& o,
@@ -3836,7 +3844,14 @@ private:
     uint64_t offset, uint64_t length,
     ceph::buffer::list& bl,
     uint32_t fadvise_flags);
-
+  int _do_write_v2_compressed(
+    TransContext *txc,
+    CollectionRef &c,
+    OnodeRef& o,
+    WriteContext& wctx,
+    uint32_t offset, uint32_t length,
+    ceph::buffer::list& bl,
+    uint32_t scan_left, uint32_t scan_right);
   int _touch(TransContext *txc,
 	     CollectionRef& c,
 	     OnodeRef& o);
