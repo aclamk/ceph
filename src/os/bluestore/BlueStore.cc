cca6e03ed7095ffa9ee8c57fa448fb0a9c91bddd
Adam Kupczyk <akupczyk@ibm.com>
2026-07-28 09:48:33 +0000

    os/bluestore: Fix compressed v2 fallback to uncompressed
    Can happen when attempted compressed write v2 has to fallback
    to uncompressed write v2.
    The uncompressed write can merges extent on left boundary,
    ignoring the fact that it might be in other shard.
    The code did not mark the other shard as dirty.
    Now properly detecting merging extents and changing affected range.
    
    Fixes: https://tracker.ceph.com/issues/78776
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -18093,11 +18093,9 @@ int BlueStore::_do_write_v2(
     }
   } else {
     // normal uncompressed path
-    BlueStore::Writer wr(this, txc, &wctx, o);
     uint64_t start = p2align(offset, min_alloc_size);
     uint64_t end = p2roundup(offset + length, min_alloc_size);
-    wr.left_affected_range = start;
-    wr.right_affected_range = end;
+    BlueStore::Writer wr(this, txc, &wctx, o);
     std::tie(wr.left_shard_bound, wr.right_shard_bound) =
       o->extent_map.fault_range_ex(db, start, end - start);
     wr.do_write(offset, bl);
@@ -18129,6 +18127,7 @@ int BlueStore::_do_write_v2_compressed(
     *_dout << i.offset << "~" << i.length << " ";
   }
   *_dout << std::dec << dendl;
+  BlueStore::Writer wr(this, txc, &wctx, o);
   for (const auto& i : regions) {
     ceph::buffer::list data_bl;
     if (i.offset <= offset && offset < i.offset + i.length) {
@@ -18156,7 +18155,6 @@ int BlueStore::_do_write_v2_compressed(
     uint32_t au_size = min_alloc_size;
     disk_for_compressed = estimator->split_and_compress(data_bl, bd);
     disk_for_raw = p2roundup(i.offset + i.length, au_size) - p2align(i.offset, au_size);
-    BlueStore::Writer wr(this, txc, &wctx, o);
     if (disk_for_compressed < disk_for_raw) {
       wr.do_write_with_blobs(i.offset, i.offset + i.length, i.offset + i.length, bd);
     } else {
@@ -18164,8 +18162,8 @@ int BlueStore::_do_write_v2_compressed(
     }
   }
   estimator->finish();
-  uint32_t changes_start = regions.front().offset;
-  uint32_t changes_end = regions.back().offset + regions.back().length;
+  uint32_t changes_start = wr.left_affected_range;
+  uint32_t changes_end = wr.right_affected_range;
   o->extent_map.compress_extent_map(changes_start, changes_end - changes_start);
   o->extent_map.dirty_range(changes_start, changes_end - changes_start);
   o->extent_map.maybe_reshard(changes_start, changes_end);
