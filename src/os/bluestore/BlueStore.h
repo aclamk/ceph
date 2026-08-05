a6230c137fcb46abd064b6c62b76be401ca28011
Adam Kupczyk <akupczyk@ibm.com>
2026-08-05 10:31:15 +0000

    code for waking up waiters
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -2298,18 +2298,14 @@ public:
 
     void flush() {
       std::unique_lock l(qlock);
+      ++kv_submitted_waiters;
       while (true) {
-	// std::set flag before the check because the condition
-	// may become true outside qlock, and we need to make
-	// sure those threads see waiters and signal qcond.
-	++kv_submitted_waiters;
-	if (q.empty() || _is_all_kv_submitted()) {
-	  --kv_submitted_waiters;
-	  return;
-	}
-	qcond.wait(l);
-	--kv_submitted_waiters;
+        if (q.empty() || _is_all_kv_submitted()) {
+          break;
+        }
+        qcond.wait_for(l, std::chrono::seconds(1));
       }
+      --kv_submitted_waiters;
     }
 
     void flush_all_but_last() {
