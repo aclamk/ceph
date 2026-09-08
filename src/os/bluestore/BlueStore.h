93cc3b2f897799d0d03c1d03805a4fa18c66b5e4
Adam Kupczyk <akupczyk@ibm.com>
2026-09-08 08:25:42 +0000

    os/bluestore: Reinstate original _read_cache, _prepare_read_ioc, _do_read
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -3423,8 +3423,16 @@ private:
     size_t length,
     int read_cache_policy,
     ready_regions_t& ready_regions,
+    blobs2read_t& blobs2read);
+
+  void _read_cache(
+    OnodeRef& o,
+    uint64_t offset,
+    size_t length,
+    int read_cache_policy,
+    ready_regions_t& ready_regions,
     blobs2read_t& blobs2read,
-    span_stat_t* span_stat = nullptr);
+    span_stat_t* span_stat);
 
   void _reformat_scan(
     OnodeRef& o,
@@ -3436,8 +3444,13 @@ private:
   int _prepare_read_ioc(
     blobs2read_t& blobs2read,
     std::vector<ceph::buffer::list>* compressed_blob_bls,
+    IOContext* ioc);
+
+  int _prepare_read_ioc(
+    blobs2read_t& blobs2read,
+    std::vector<ceph::buffer::list>* compressed_blob_bls,
     IOContext* ioc,
-    span_stat_t* span_stat = nullptr);
+    span_stat_t* span_stat);
 
   int _generate_read_result_bl(
     OnodeRef& o,
@@ -3461,8 +3474,17 @@ private:
     size_t len,
     ceph::buffer::list& bl,
     uint32_t op_flags = 0,
-    uint64_t retry_count = 0,
-    span_stat_t* span_stat = nullptr);
+    uint64_t retry_count = 0);
+
+  int _do_read(
+    Collection *c,
+    OnodeRef& o,
+    uint64_t offset,
+    size_t len,
+    ceph::buffer::list& bl,
+    uint32_t op_flags,
+    uint64_t retry_count,
+    span_stat_t* span_stat);
 
   void _do_read_and_pad(
     Collection* c,
