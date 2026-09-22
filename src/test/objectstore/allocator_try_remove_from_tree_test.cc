b64a8dea96a763993fad0c7de083db0429ccbfda
Adam Kupczyk <akupczyk@ibm.com>
2026-09-22 08:01:48 +0000

    test/objectstore/allocator: Add thrash tests for avl, btree, btree2
    The idea is to explore paths that were too convoluted to craft decicated
    unit tests.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/test/objectstore/allocator_try_remove_from_tree_test.cc
+++ src/test/objectstore/allocator_try_remove_from_tree_test.cc
@@ -14,6 +14,10 @@
 #include "os/bluestore/AvlAllocator.h"
 #include "os/bluestore/BtreeAllocator.h"
 #include "os/bluestore/Btree2Allocator.h"
+#include "os/bluestore/simple_bitmap.h"
+#include <random>
+
+using namespace std;
 
 namespace {
 
@@ -311,4 +315,138 @@ TEST(Btree2Allocator, try_remove_leftover_split_makes_progress)
   EXPECT_EQ((std::vector<l_extent_t>{
     {0, 50 * _64k},
   }), extents(a));
+}
+
+
+class AllocRandom : virtual public ::testing::Test {
+public:
+
+  void SetUp() override {};
+  void TearDown() override {};
+
+  void foreach_extent(
+      SimpleBitmap* b,
+      uint64_t start,
+      uint64_t len,
+      std::function<void(uint64_t /*pos*/, uint64_t /*len*/, bool /*exists*/)> cb)
+  {
+    uint64_t pos = start;
+    uint64_t end = start + len;
+    extent_t t;
+    do {
+      t = b->get_next_set_extent(pos);
+      if (t.length == 0) {
+        if (pos < end) {
+          cb(pos, end - pos, false);
+        }
+        break;
+      }
+      if (pos < t.offset) {
+        uint64_t eend = std::min(end, t.offset);
+        cb(pos, eend - pos, false);
+      }
+      if (t.offset >= end) {
+        break;
+      }
+      uint64_t eend = std::min(end, t.offset + t.length);
+      cb(t.offset, eend - t.offset, true);
+      pos = t.offset + t.length;
+    } while (pos < end);
+  }
+  template<typename alloc_type>
+  void random_test(alloc_type& a, size_t size)
+  {
+    static constexpr bool debug = false;
+    uint32_t insert_size = 30;
+    uint32_t remove_size = 20;
+    uint32_t list_size = 200;
+
+    uniform_int_distribution<> insert_gen = uniform_int_distribution<>(1, insert_size);
+    uniform_int_distribution<> remove_gen = uniform_int_distribution<>(1, remove_size);
+    uniform_int_distribution<> list_gen = uniform_int_distribution<>(1, list_size);
+
+    typedef std::mt19937 gen_type;
+    gen_type rng(0);
+
+    //TestAvlAllocator a(g_ceph_context, size, 1, "avl");
+    //alloc_type a(g_ceph_context, size, 1, "tested");
+    SimpleBitmap b(g_ceph_context, size);
+    for (int i = 0; i < 20000; i++) {
+      {
+        //insert phase
+        uint64_t l = insert_gen(rng);
+        uint64_t o = uniform_int_distribution<>(0, size - l)(rng);
+        std::vector<l_extent_t> to_fill;
+        foreach_extent(&b, o, l, [&](uint64_t pos, uint64_t len, bool exists) {
+          if (!exists)
+            a.init_add_free(pos, len);
+        });
+        b.set(o, l);
+        if (debug) std::cout << "SET " << o << "~" << l << std::endl;
+      }
+      {
+        //remove phase
+        uint64_t l = remove_gen(rng);
+        uint64_t o = uniform_int_distribution<>(0, size - l)(rng);
+        std::vector<l_extent_t> to_remove;
+        foreach_extent(&b, o, l, [&](uint64_t pos, uint64_t len, bool exists) {
+          if (exists)
+            a.init_rm_free(pos, len);
+        });
+        b.clr(o, l);
+        if (debug) std::cout << "CLR " << o << "~" << l << std::endl;
+      }
+      {
+        //list phase
+        uint64_t l = list_gen(rng);
+        uint64_t o = uniform_int_distribution<>(0, size - l)(rng);
+        if (debug) std::cout << "range=" << o << "~" << l << std::endl;
+        std::vector<l_extent_t> extents_bitmap;
+        std::vector<l_extent_t> extents_avl;
+        foreach_extent(&b, o, l, [&](uint64_t pos, uint64_t len, bool exists) {
+          if (exists)
+            extents_bitmap.emplace_back(pos, len);
+        });
+        a._try_remove_from_tree(o, l, [&](uint64_t pos, uint64_t len, bool exists) {
+          if (exists)
+            extents_avl.emplace_back(pos, len);
+        });
+        if (debug) {
+          for (auto& x : extents_bitmap) {
+            std::cout << "[" << x.offset << "~" << x.length << "],";
+          }
+          std::cout << std::endl;
+          for (auto& x : extents_avl) {
+            std::cout << "(" << x.offset << "~" << x.length << "),";
+          }
+          std::cout << std::endl;
+        }
+        ASSERT_EQ(extents_bitmap, extents_avl);
+
+
+        // give back extents
+        for (auto& x : extents_avl) {
+          a.init_add_free(x.offset, x.length);
+        }
+      }
+    }
+  }
+};
+
+TEST_F(AllocRandom, avl) {
+  size_t size = 100000;
+  TestAvlAllocator a(g_ceph_context, size, 1, "avl");
+  random_test<TestAvlAllocator>(a, size);
+}
+
+TEST_F(AllocRandom, btree) {
+  size_t size = 100000;
+  TestBtreeAllocator a(g_ceph_context, size, 1, "btree");
+  random_test<TestBtreeAllocator>(a, size);
+}
+
+TEST_F(AllocRandom, btree2) {
+  size_t size = 100000;
+  TestBtree2Allocator a(g_ceph_context, size, 1, max_mem, 1.0, false, "btree2");
+  random_test<TestBtree2Allocator>(a, size);
 }
